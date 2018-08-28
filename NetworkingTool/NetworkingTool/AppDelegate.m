09030dcb484fede2fa681064b7d0a9d44242df39
yaomin <237347824@qq.com>
2018-08-28 14:21:30 +0800

    test

--- NetworkingTool/NetworkingTool/AppDelegate.m
+++ NetworkingTool/NetworkingTool/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // test
     [kNetworingTool setUpConfig:^(NetworkingConfig *config) {
         config.generalServer=@"";
         config.showRequestLog=YES;
